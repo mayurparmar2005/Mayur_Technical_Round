7238071aadd45f12482d85607725494811b16cc8
mayurparmar2005 <mayurparmar5054@gmail.com>
2026-07-20 12:19:13 +0530

    DuplicateElement Array entry has improved

--- CodingRound/DuplicateElement.java
+++ CodingRound/DuplicateElement.java
@@ -7,9 +7,10 @@ public class DuplicateElement {
         int n = sc.nextInt();
         int arr[] = new int[n];
         System.out.println("Enter the elements of the array: ");
-        for(int l = 0; l < n; l++) {
-            arr[l] = sc.nextInt();
-        }
+        for(int l = 0 ; l < n ; l++){
+			System.out.print("Enter arr["+l+"] :");
+			arr[l] = sc.nextInt();
+		}
         System.out.println("Duplicate elements in given array: ");
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j < n; j++) {
