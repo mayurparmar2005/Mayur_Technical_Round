a0d7cfe203842699bdd4d0ca101ad38af15b3df0
mayurparmar2005 <mayurparmar5054@gmail.com>
2026-07-20 12:06:22 +0530

    DuplicateElement in array

---
+++ CodingRound/DuplicateElement.java
@@ -0,0 +1,23 @@
+package CodingRound;
+import java.util.Scanner;
+public class DuplicateElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int l = 0; l < n; l++) {
+            arr[l] = sc.nextInt();
+        }
+        System.out.println("Duplicate elements in given array: ");
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    System.out.println(arr[j]);
+                }
+            }
+        }
+        sc.close();
+    }
+}
